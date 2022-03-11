c4da30a131d09ad41bf2d8cdc4961d41fc8dacda
Mihaita-Placinta <92861420+Mihaita-Placinta@users.noreply.github.com>
2022-03-11 11:18:14 +0200

    changed
    test

---
+++ pokedexapp/src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import React from 'react';
+import Data from "./data.json";
+import { useState } from 'react';
+function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+  return (
+    <div className='title'>
+      <h3>Pokedex </h3>
+      <div className="container">
+        <input type="text" placeholder="seach..." onChange={e => setSearchTerm(e.target.value)} className="search" />
+        {Data.filter((id) => {
+          if (searchTerm === "") {
+            return id
+          }
+          else if (
+            id.name.toLowerCase().includes(searchTerm.toLowerCase())) {
+            return id;
+          }
+        }).map((id, key) => {
+          return <div key={id.id} className="container-card">
+            <h3> {id.id} </h3>
+            <h4> {id.name}</h4>
+            <div className="pokemonPhoto">
+              <img src={id.sprites.other.official_artwork.front_default} />
+            </div>
+          </div>
+        })}
+      </div>
+    </div>);
+} export default App;
